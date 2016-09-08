39efe84ac3ab7c4f4515efe67891c6e774542d0d
Kaleb Lape <kaleb.lape@metova.com>
2016-09-08 13:15:33 -0500

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+var HipChat = require('hipchat-message');
+
+var hipchatAuthToken = 'lgdIDCBWU21B1ii4PNQc59Z0mjANbnUVxUUzbT0f';
+var hipchatRoomId = '1311730';
+
+exports.handler = function (event, context) {
+  var jsonMessage = event.Records[0].Sns.Message;
+  console.log(jsonMessage);
+  var alarm = JSON.parse(jsonMessage);
+
+  var hipchat = new HipChat({
+      'auth_token': hipchatAuthToken,
+      'room_id'   : hipchatRoomId,
+      'from'      : 'AWS CloudWatch',
+      title       : `<strong>${alarm.AlarmName: alarm.NewStateValue}</strong>`,
+      format      : 'html',
+  }, function () {
+      console.log('call completed');
+  });
+
+  var alarmState = alarm.NewStateValue === 'ALARM' ? 'error' : 'success';
+  hipchat[alarmState](alarm.NewStateReason, true, function (error, responseCode, body) {
+    var requestState = error ? 'fail' : 'succeed';
+    context[requestState](body);
+  });
+};
