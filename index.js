0ea36da19db1fdafbb686a5f6e9a0976d1ec2090
Kaleb Lape <kaleb.lape@metova.com>
2016-09-08 23:09:46 -0500

    Use template to render notification.

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const fs = require('fs');
 const path = require('path');
+const Handlebars = require('handlebars');
 const HipChatNotifierMaker = require('hipchat-notifier');
 
-var config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'), 'utf8'));
+const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'), 'utf8'));
+const messageTemplateSource = fs.readFileSync(path.join(__dirname, 'message.html.handlebars'), 'utf8');
+const messageTemplate = Handlebars.compile(messageTemplateSource);
+const notifier = HipChatNotifierMaker.make(config['room_id'], config['auth_token'], 'AWS CloudWatch');
 
 exports.handler = function (event, context) {
-  console.log(config);
   var jsonMessage = event.Records[0].Sns.Message;
   console.log(jsonMessage);
-  var alarm = JSON.parse(jsonMessage);
 
-  notifier = HipChatNotifierMaker.make(config['room_id'], config['auth_token'], 'AWS CloudWatch');
+  var alarm = JSON.parse(jsonMessage);
 
   var alarmState = alarm.NewStateValue === 'ALARM' ? 'failure' : 'success';
 
-  var message = `<p><strong>${alarm.AlarmName}: ${alarm.NewStateValue}</strong></p><p>${alarm.NewStateReason}</p>`
+  var message = messageTemplate(alarm);
+
   notifier[alarmState](message, function (error, response, body) {
     var requestState = error ? 'fail' : 'succeed';
     context[requestState](body);
