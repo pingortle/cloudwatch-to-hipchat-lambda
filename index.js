70fd20039e33e176c2a50818b08749e11284da48
Kaleb Lape <kaleb.lape@metova.com>
2016-09-11 11:41:53 -0500

    Use const.

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ const messageTemplate = Handlebars.compile(messageTemplateSource);
 const notifier = HipChatNotifierMaker.make(config['room_id'], config['auth_token'], 'AWS CloudWatch');
 
 exports.handler = function (event, context) {
-  var jsonMessage = event.Records[0].Sns.Message;
+  const jsonMessage = event.Records[0].Sns.Message;
   console.log(jsonMessage);
 
-  var alarm = JSON.parse(jsonMessage);
+  const alarm = JSON.parse(jsonMessage);
 
-  var alarmState = alarm.NewStateValue === 'ALARM' ? 'failure' : 'success';
+  const alarmState = alarm.NewStateValue === 'ALARM' ? 'failure' : 'success';
 
-  var message = messageTemplate(alarm);
+  const message = messageTemplate(alarm);
 
   notifier[alarmState](message, function (error, response, body) {
-    var requestState = error ? 'fail' : 'succeed';
+    const requestState = error ? 'fail' : 'succeed';
     context[requestState](body);
   });
 };
