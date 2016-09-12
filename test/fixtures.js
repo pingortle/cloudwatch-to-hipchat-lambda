96d97ff81acd0a2b85acdd96aab9ce9dd69fc692
Kaleb Lape <kaleb.lape@metova.com>
2016-09-11 20:13:22 -0500

    Lint tests.

--- test/fixtures.js
+++ test/fixtures.js
@@ -1,55 +1,55 @@
 exports.elbAlarmMessageData = {
-    "AlarmName": "awselb-Unhealthy-Hosts",
-    "AlarmDescription": "Created from EC2 Console",
-    "AWSAccountId": "293228007553",
-    "NewStateValue": "ALARM",
-    "NewStateReason": "Threshold Crossed: 1 datapoint (6.0) was greater than or equal to the threshold (1.0).",
-    "StateChangeTime": "2016-09-10T03:25:51.212+0000",
-    "Region": "US West - Oregon",
-    "OldStateValue": "OK",
-    "Trigger": {
-        "MetricName": "UnHealthyHostCount",
-        "Namespace": "AWS/ELB",
-        "Statistic": "SUM",
-        "Unit": null,
-        "Dimensions": [
-            {
-                "name": "LoadBalancerName",
-                "value": "production"
-            }
-        ],
-        "Period": 300,
-        "EvaluationPeriods": 1,
-        "ComparisonOperator": "GreaterThanOrEqualToThreshold",
-        "Threshold": 1
-    }
+  'AlarmName': 'awselb-Unhealthy-Hosts',
+  'AlarmDescription': 'Created from EC2 Console',
+  'AWSAccountId': '293228007553',
+  'NewStateValue': 'ALARM',
+  'NewStateReason': 'Threshold Crossed: 1 datapoint (6.0) was greater than or equal to the threshold (1.0).',
+  'StateChangeTime': '2016-09-10T03:25:51.212+0000',
+  'Region': 'US West - Oregon',
+  'OldStateValue': 'OK',
+  'Trigger': {
+    'MetricName': 'UnHealthyHostCount',
+    'Namespace': 'AWS/ELB',
+    'Statistic': 'SUM',
+    'Unit': null,
+    'Dimensions': [
+      {
+        'name': 'LoadBalancerName',
+        'value': 'production'
+      }
+    ],
+    'Period': 300,
+    'EvaluationPeriods': 1,
+    'ComparisonOperator': 'GreaterThanOrEqualToThreshold',
+    'Threshold': 1
+  }
 };
 
 exports.elbOkMessageData = {
-    "AlarmName": "awselb-Unhealthy-Hosts",
-    "AlarmDescription": "Created from EC2 Console",
-    "AWSAccountId": "293228007553",
-    "NewStateValue": "OK",
-    "NewStateReason": "Threshold Crossed: 1 datapoint (6.0) was greater than or equal to the threshold (1.0).",
-    "StateChangeTime": "2016-09-10T03:25:51.212+0000",
-    "Region": "US West - Oregon",
-    "OldStateValue": "ALARM",
-    "Trigger": {
-        "MetricName": "UnHealthyHostCount",
-        "Namespace": "AWS/ELB",
-        "Statistic": "SUM",
-        "Unit": null,
-        "Dimensions": [
-            {
-                "name": "LoadBalancerName",
-                "value": "production"
-            }
-        ],
-        "Period": 300,
-        "EvaluationPeriods": 1,
-        "ComparisonOperator": "GreaterThanOrEqualToThreshold",
-        "Threshold": 1
-    }
+  'AlarmName': 'awselb-Unhealthy-Hosts',
+  'AlarmDescription': 'Created from EC2 Console',
+  'AWSAccountId': '293228007553',
+  'NewStateValue': 'OK',
+  'NewStateReason': 'Threshold Crossed: 1 datapoint (6.0) was greater than or equal to the threshold (1.0).',
+  'StateChangeTime': '2016-09-10T03:25:51.212+0000',
+  'Region': 'US West - Oregon',
+  'OldStateValue': 'ALARM',
+  'Trigger': {
+    'MetricName': 'UnHealthyHostCount',
+    'Namespace': 'AWS/ELB',
+    'Statistic': 'SUM',
+    'Unit': null,
+    'Dimensions': [
+      {
+        'name': 'LoadBalancerName',
+        'value': 'production'
+      }
+    ],
+    'Period': 300,
+    'EvaluationPeriods': 1,
+    'ComparisonOperator': 'GreaterThanOrEqualToThreshold',
+    'Threshold': 1
+  }
 };
 
 exports.snsElbAlarmEvent = generateSnsElbEvent(exports.elbAlarmMessageData);
@@ -57,32 +57,32 @@ exports.snsElbOkEvent = generateSnsElbEvent(exports.elbOkMessageData);
 
 function generateSnsElbEvent(messageData) {
   return {
-    "Records": [
+    'Records': [
       {
-        "EventVersion": "1.0",
-        "EventSubscriptionArn": "arn:aws:sns:EXAMPLE",
-        "EventSource": "aws:sns",
-        "Sns": {
-          "SignatureVersion": "1",
-          "Timestamp": "1970-01-01T00:00:00.000Z",
-          "Signature": "EXAMPLE",
-          "SigningCertUrl": "EXAMPLE",
-          "MessageId": "95df01b4-ee98-5cb9-9903-4c221d41eb5e",
-          "Message": JSON.stringify(messageData),
-          "MessageAttributes": {
-            "Test": {
-              "Type": "String",
-              "Value": "TestString"
+        'EventVersion': '1.0',
+        'EventSubscriptionArn': 'arn:aws:sns:EXAMPLE',
+        'EventSource': 'aws:sns',
+        'Sns': {
+          'SignatureVersion': '1',
+          'Timestamp': '1970-01-01T00:00:00.000Z',
+          'Signature': 'EXAMPLE',
+          'SigningCertUrl': 'EXAMPLE',
+          'MessageId': '95df01b4-ee98-5cb9-9903-4c221d41eb5e',
+          'Message': JSON.stringify(messageData),
+          'MessageAttributes': {
+            'Test': {
+              'Type': 'String',
+              'Value': 'TestString'
             },
-            "TestBinary": {
-              "Type": "Binary",
-              "Value": "TestBinary"
+            'TestBinary': {
+              'Type': 'Binary',
+              'Value': 'TestBinary'
             }
           },
-          "Type": "Notification",
-          "UnsubscribeUrl": "EXAMPLE",
-          "TopicArn": "arn:aws:sns:EXAMPLE",
-          "Subject": "TestInvoke"
+          'Type': 'Notification',
+          'UnsubscribeUrl': 'EXAMPLE',
+          'TopicArn': 'arn:aws:sns:EXAMPLE',
+          'Subject': 'TestInvoke'
         }
       }
     ]
